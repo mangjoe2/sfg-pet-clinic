2969a530b167487f476cf9d69d7b9ec97bd6c958
mama <mariejoseph144@gmail.com>
2020-09-08 03:29:56 -0400

    ADDING POJOs. Classes #2

--- src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,9 +1,14 @@
 package guru.springframework.sfgpetclinic;
 
+import org.junit.runner.RunWith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootApplication
+@RunWith(SpringRunner.class)
+@SpringBootTest
+//@SpringBootApplication
 public class SfgPetClinicApplication {
 
     public static void main(String[] args) {
