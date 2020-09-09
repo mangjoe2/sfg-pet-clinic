e9d614e7635083cb26a87f2f036b6dcb62562f9d
mama <mariejoseph144@gmail.com>
2020-09-09 18:22:42 -0400

    Implement Interfaces for services. Closes #5

--- pet-clinic-web/src/main/java/model/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/model/SfgPetClinicApplication.java
@@ -1,8 +1,7 @@
-import org.junit.runner.RunWith;
+package model;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 //@RunWith(SpringRunner.class)
 //@SpringBootTest
