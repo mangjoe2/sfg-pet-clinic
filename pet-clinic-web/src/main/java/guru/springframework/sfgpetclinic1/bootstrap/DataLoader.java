d5d052f7abaa6a3e3fc135eadf39d2b260e6ee25
wiyaou mangazie <mariejoseph144@gmail.com>
2020-09-24 05:08:54 -0400

    Added PetType to bootstrap. closes #33

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic1/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic1/bootstrap/DataLoader.java
@@ -43,28 +43,7 @@ public class DataLoader implements CommandLineRunner {
 
         Owner owner2 = new Owner();
 
-        owner2.setFirstName("Wiyaou");
-        owner2.setLastName("Daruises");
 
-        ownerService.save(owner2);
-
-        System.out.println("Loaded Owners....");
-
-        Vet vet1 = new Vet();
-
-        vet1.setFirstName("Jockes");
-        vet1.setLastName("Sam");
-
-        vetService.save(vet1);
-
-        Vet vet2 = new Vet();
-
-        vet2.setFirstName("Malin");
-        vet2.setLastName("Dao");
-
-        vetService.save(vet2);
-
-        System.out.println("Loaded Vets ....");
 
 
     }
