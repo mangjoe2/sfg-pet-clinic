04e90da3d1a3fb854a384893e675781fced37482
wiyaou mangazie <mariejoseph144@gmail.com>
2020-09-28 02:54:35 -0400

    Added visit Entity. Closes #39

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic1/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic1/model/Pet.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic1.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -10,7 +12,7 @@ public class Pet extends  BaseEntity{
     @Column(name = "name")
     private String name;
 
-    @Column(name = "pettype")
+    @ManyToOne
     @JoinColumn(name = "type_id")
     private PetType petType;
 
@@ -20,6 +22,9 @@ public class Pet extends  BaseEntity{
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -50,5 +55,13 @@ public class Pet extends  BaseEntity{
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
