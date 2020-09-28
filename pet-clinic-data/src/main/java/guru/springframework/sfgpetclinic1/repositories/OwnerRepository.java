a7f009a2a1f729bdca8ac37a4a3412890117b381
wiyaou mangazie <mariejoseph144@gmail.com>
2020-09-28 03:30:30 -0400

    Added Spring data jpa service owners. Closes #41

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic1/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic1/repositories/OwnerRepository.java
@@ -4,4 +4,5 @@ import guru.springframework.sfgpetclinic1.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
