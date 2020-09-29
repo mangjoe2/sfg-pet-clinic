92b42973d2759316f2c005ea8811707ac4e5949b
wiyaou mangazie <mariejoseph144@gmail.com>
2020-09-29 15:49:57 -0400

    Added petType SD JPA Service. Cloese #43

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic1/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,43 @@
+package guru.springframework.sfgpetclinic1.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic1.model.Vet;
+import guru.springframework.sfgpetclinic1.repositories.VetRepository;
+import guru.springframework.sfgpetclinic1.services.VetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class VetSDJpaService implements VetService {
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
