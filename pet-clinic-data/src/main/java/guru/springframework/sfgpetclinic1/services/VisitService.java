7c89fd24d2bbac9ba842a76b38d2254487641f12
wiyaou mangazie <mariejoseph144@gmail.com>
2020-09-29 18:29:12 -0400

    Implemented Map Impl for visit. Close #47

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic1/services/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic1.services;
+
+import guru.springframework.sfgpetclinic1.model.Visit;
+
+public interface VisitService extends CrudeService<Visit, Long> {
+}
